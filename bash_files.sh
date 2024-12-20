923559c596d85cd3e01ee1d6df9a7c530d7cbdfa
sis-I <sisshash@gmail.com>
2024-12-20 18:45:13 +0300

    add bash_files.sh

--- bash_files.sh
+++ bash_files.sh
@@ -2,6 +2,13 @@ echo "Install Dependencies"
 
 pip install -r requirements.txt
 
+
+echo "Model migrations into database"
+
+python3.12 manage.py makemigrations
+python3.12 manage.py migrate
+
+
 echo "Collect all static files" 
 
 python3.12 manage.py collectstatic

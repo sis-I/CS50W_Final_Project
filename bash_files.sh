3e746435aeb277d038b5f4ada65aa9da00222013
sis-I <sisshash@gmail.com>
2024-12-20 18:52:58 +0300

    remove staticfiles_build

--- bash_files.sh
+++ bash_files.sh
@@ -2,13 +2,12 @@ echo "Install Dependencies"
 
 pip install -r requirements.txt
 
-
 echo "Model migrations into database"
 
-python3.12 manage.py makemigrations
-python3.12 manage.py migrate
+python3 manage.py makemigrations
+python3 manage.py migrate
 
 
 echo "Collect all static files" 
 
-python3.12 manage.py collectstatic
+python3 manage.py collectstatic --noinput
